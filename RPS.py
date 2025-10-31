86904fbb0246c0f8b99fc486b7aadfe344a75b06
TheUnknown550 <mattcosh06@gmail.com>
2025-11-01 01:31:50 +0700

    Beat Abby with 99%

--- RPS.py
+++ RPS.py
@@ -1,4 +1,5 @@
 import random
+from collections import defaultdict
 
 def player(prev_play, opponent_history=[], my_history=[]):
     # Default move
@@ -40,8 +41,28 @@ def player(prev_play, opponent_history=[], my_history=[]):
     else:
         # Kris opens with 'P' (since he assumes our '' -> 'R'), so play 'S'
         guess = 'S'
+        
+    # Strategy Abbey: Uses 2-move Markov prediction
+    if my_history:
+        # Build transition counts: XY means we played X then Y
+        play_order = defaultdict(int)
+        for i in range(len(my_history) - 1):
+            pair = my_history[i] + my_history[i + 1]
+            play_order[pair] += 1
+        last_my = my_history[-1]
+    else:
+        # Abbey treats empty prev as 'R'
+        last_my = 'R'
+        play_order = {}
+    potential_plays = [last_my + 'R', last_my + 'P', last_my + 'S']
+    best_pair = max(potential_plays, key=lambda k: play_order.get(k, 0))
+    predicted_our_next = best_pair[-1]
+    abbey_next = counter[predicted_our_next]
+    guess = counter[abbey_next]
 
-    
+
     # Track our own move history for strategies that need it
     my_history.append(guess)
+    
+    
     return guess
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from RPS import player
 from unittest import main
 
 # play(player, quincy, 1000)
-# play(player, abbey, 1000)
-play(player, kris, 1000)
+play(player, abbey, 1000)
+# play(player, kris, 1000)
 # play(player, mrugesh, 1000)
 
 # Uncomment line below to play interactively against a bot:
